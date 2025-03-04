4ef9e875ea51d435388a8312e6881fdcc354052a
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-03-03 18:00:10 -0800

    Adding typeahead solution

--- src/solutions/typeahead/typeahead.jsx
+++ src/solutions/typeahead/typeahead.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import _ from "lodash";
 import "./typeahead.css";
-const sampleData = [
+const productList = [
   "Apple",
   "Banana",
   "Blueberry",
@@ -15,34 +16,124 @@ const sampleData = [
   "Grape",
   "Grapefruit",
   "Guava",
+  "Lime",
+  "Honeydew",
+  "Jackfruit",
   "Kiwi",
   "Lemon",
-  "Lime",
   "Mango",
-  "Melon",
+  "Nectarine",
   "Orange",
   "Papaya",
-  "Peach",
-  "Pear",
-  "Pineapple",
-  "Plum",
+  "Quince",
   "Raspberry",
   "Strawberry",
   "Watermelon",
 ];
 const Typeahead = () => {
   const [input, setInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const debouncedSearchProductList = React.useCallback(
+    _.debounce((value) => {
+      if (!value) return;
+      const filteredSuggestions = productList.filter((suggestion) =>
+        suggestion.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+      setShowSuggestions(true);
+    }, 500),
+    []
+  );
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInput(event.target.value);
+
+    if (!value) {
+      setSuggestions([]);
+      setShowSuggestions(false);
+      return;
+    }
+    //debounce search
+    debouncedSearchProductList(value);
+  };
+
+  const handleSelectedSuggestion = (suggestion) => {
+    setInput(suggestion);
+    setSuggestions([]);
+    setShowSuggestions(false);
+  };
+
+  const handleBlur = () => {
+    setShowSuggestions(false);
+  };
+
+  const handleFocus = () => {
+    console.log("handle focus");
+    setSelectedIndex(0);
+    setShowSuggestions(true);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      setShowSuggestions(false);
+      handleSelectedSuggestion(suggestions[selectedIndex]);
+    }
+    if (event.key === "Escape") {
+      event.preventDefault();
+      setShowSuggestions(false);
+    }
+    if (event.key === "ArrowDown") {
+      event.preventDefault();
+      setSelectedIndex((prevIndex) =>
+        prevIndex < suggestions.length - 1 ? prevIndex + 1 : 0
+      );
+      setShowSuggestions(true);
+    }
+
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setSelectedIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : suggestions.length - 1
+      );
+      setShowSuggestions(true);
+    }
+  };
 
   return (
-    <div>
+    <div className="typeahead">
       <input
-        className="typeahead"
+        className="typeahead-input"
         type="text"
         name="typeahead"
         placeholder="Search"
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+        onKeyDown={handleKeyDown}
+        aria-label="Search Products"
       />
+      {showSuggestions && (
+        <div className="typeahead-dropdown">
+          <ul>
+            {suggestions.map((suggestion, index) => (
+              <li
+                className={index === selectedIndex ? "selected" : ""}
+                onClick={() => handleSelectedSuggestion(suggestion)}
+                key={index}
+                aria-selected={index === selectedIndex}
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
